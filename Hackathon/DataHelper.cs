541b170ffd966cb3d34ce9b68fe85bb7842f9e7a
Hans Kilian <hkkilian@gmail.com>
2017-01-18 11:05:45 +0100

    First iteration

---
+++ Hackathon/DataHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    public class DataHelper
+    {
+        public static string GetString(string[][] data, string column, int row)
+        {
+            var columnNumber = Array.IndexOf(data[0], column);
+            return data[row][columnNumber];
+        }
+
+        public static double[][] CreateInputs(string[][] data)
+        {
+            var random = new Random();
+            List<double[]> result = new List<double[]>();
+            for (int i = 1; i < data.Length; i++)
+            {
+                double expected = GetString(data, "FINANCIAL_DISTRESS", i) == "YES" ? 1.0 : 0.0;
+                result.Add(new double[] { expected, random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble() });
+            }
+            return result.ToArray();
+        }
+
+        public static double[][] CreateOutputs(string[][] data)
+        {
+            List<double[]> result = new List<double[]>();
+            for (int i = 1; i < data.Length; i++)
+            {
+                double expected = GetString(data, "FINANCIAL_DISTRESS", i) == "YES" ? 1.0 : 0.0;
+                result.Add(new double[] { expected });
+            }
+            return result.ToArray();
+        }
+    }
+}
--- Hackathon/Program.cs
+++ Hackathon/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Accord.Neuro;
+using Accord.Neuro.Learning;
 
 namespace Hackathon
 {
@@ -10,9 +12,21 @@ namespace Hackathon
     {
         static void Main(string[] args)
         {
-            var csvReader = new CsvReader(@"HACKATHON_2_CREDIT_MODEL_INPUTS.csv");
+            var csvReader = new CsvReader(@"..\..\..\HACKATHON_2_CREDIT_MODEL_INPUTS_TRAINING.csv");
             var data = csvReader.ReadFile();
-            Console.WriteLine("test");
+            var inputs = DataHelper.CreateInputs(data);
+            var outputs = DataHelper.CreateOutputs(data);
+            double error = 0.0;
+
+            ActivationNetwork network = new ActivationNetwork(new SigmoidFunction(), 10, 10, 1);
+            var teacher = new ParallelResilientBackpropagationLearning(network);
+            teacher.Reset(0.1);
+            for (int i = 0; i < 100; i++)
+            {
+                error = teacher.RunEpoch(inputs, outputs);
+            }
+
+            Console.WriteLine(error);
         }
 
 
